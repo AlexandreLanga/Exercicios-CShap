e7874a66115b8be409be02a78e99672b3a731fa1
Alexandre Langa <alelanga424@hotmail.com>
2022-04-27 22:36:11 -0300

    main

---
+++ MeusExercicios/MinhasInformacoes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MinhasInformacoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string nome;
+            int idade;
+            double altura;
+            double peso;
+
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Sua idade e:");
+            int.TryParse(Console.ReadLine(), out idade);
+
+            Console.WriteLine("Sua altura e");
+            Double.TryParse(Console.ReadLine(), out altura);
+
+            Console.WriteLine("Seu peso e:");
+            Double.TryParse(Console.ReadLine(), out peso);
+
+           
+        }
+    }
+}
