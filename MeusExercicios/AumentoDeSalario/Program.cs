0d9c5f9c270704c50e1d93116955fde112c5c0e0
Alexandre Langa <alelanga424@hotmail.com>
2022-05-04 17:09:30 -0300

    Em casa

---
+++ MeusExercicios/AumentoDeSalario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AumentoDeSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Salário	Percentual de Reajuste
+            0 - 400.00 15%
+            400.01 - 800.00 12%
+            800.01 - 1200.00 10%
+            1200.01 - 2000.00 7%
+            Acima de 2000.00 4%*/
+
+            double salario, reajuste, percentual;
+
+
+            Double.TryParse(Console.ReadLine(), out salario);
+
+            if (0 <= salario && salario <= 400)
+            { 
+
+            }
+
+        }
+    }
+}
