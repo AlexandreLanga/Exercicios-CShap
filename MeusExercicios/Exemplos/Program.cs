3c008ed31e9a5388517a019f8a26c7e188a1dd2e
AlexandreLanga <alelanga424@hotmail.com>
2022-05-03 22:19:02 -0300

    Loop

---
+++ MeusExercicios/Exemplos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exemplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1 = 0, numero2 = 0, resultado = 0;
+            int contador;
+
+            contador = 0;
+            numero1 = 0;
+
+            for (contador = 0, numero1 = 0; contador <= 100; contador += 2)
+            {
+                if (contador % 2 == 0)
+                {
+                    numero1 = contador * 3;
+                }
+                else
+                {
+                    numero2 = contador + 9;
+                }
+            }
+        }
+    }
+}
