c8a8dc262bf9ec0350c835bf507c9d06ecb84d54
Alexandre Langa <alelanga424@hotmail.com>
2022-05-05 17:00:48 -0300

    problemas

--- MeusExercicios/AumentoDeSalario/Program.cs
+++ MeusExercicios/AumentoDeSalario/Program.cs
@@ -13,16 +13,65 @@ namespace AumentoDeSalario
             1200.01 - 2000.00 7%
             Acima de 2000.00 4%*/
 
-            double salario, reajuste, percentual;
+            double salario, reajuste;
+            int percentual;
+            const double aumento1 = 15, aumento2 = 12, aumento3 = 10, aumento4 = 7, aumento5 = 4;
+
+            reajuste = 0;
 
 
+            Console.WriteLine("Digite seu salário:");
             Double.TryParse(Console.ReadLine(), out salario);
 
             if (0 <= salario && salario <= 400)
-            { 
+            {
+                reajuste = (((salario * aumento1) / 100) + salario);
+                percentual = 15;
 
+                Console.WriteLine("Novo salario:"); Console.WriteLine(reajuste);
+                Console.WriteLine("Reajuste ganho:"); Console.WriteLine(reajuste - salario);
+                Console.WriteLine("Em percentual:"); Console.WriteLine(percentual + "%");
             }
 
+            else if (400.01 <= salario && salario <= 800)
+            {
+                reajuste = (((salario * aumento2) / 100) + salario);
+                percentual = 12;
+
+                Console.WriteLine("Novo salario:"); Console.WriteLine(reajuste);
+                Console.WriteLine("Reajuste ganho:"); Console.WriteLine(reajuste - salario);
+                Console.WriteLine("Em percentual:"); Console.WriteLine(percentual + "%");
+            }
+
+            else if (800.01 <= salario && salario <= 1200)
+            {
+                reajuste = (((salario * aumento3) / 100) + salario);
+                percentual = 10;
+
+                Console.WriteLine("Novo salario:"); Console.WriteLine(reajuste);
+                Console.WriteLine("Reajuste ganho:"); Console.WriteLine(reajuste - salario);
+                Console.WriteLine("Em percentual:"); Console.WriteLine(percentual + "%");
+            }
+
+            else if (1200.01 <= salario && salario <= 2000)
+            {
+                reajuste = (((salario * aumento4) / 100) + salario);
+                percentual = 7;
+
+                Console.WriteLine("Novo salario:"); Console.WriteLine(reajuste);
+                Console.WriteLine("Reajuste ganho:"); Console.WriteLine(reajuste - salario);
+                Console.WriteLine("Em percentual:"); Console.WriteLine(percentual + "%");
+            }
+
+            else
+            {
+                reajuste = (((salario * aumento5) / 100) + salario);
+                percentual = 4;
+
+                Console.WriteLine("Novo salario:"); Console.WriteLine(reajuste);
+                Console.WriteLine("Reajuste ganho:"); Console.WriteLine(reajuste - salario);
+                Console.WriteLine("Em percentual:"); Console.WriteLine(percentual + "%");
+            }
         }
     }
 }
--- MeusExercicios/Intervalo/Program.cs
+++ MeusExercicios/Intervalo/Program.cs
@@ -13,22 +13,22 @@ namespace Intervalo
 
             if (0 <= valor && valor <= 25)
             {
-                Console.WriteLine("Intervalo (0,25]");
+                Console.WriteLine("Intervalo (0, 25]");
             }
 
             else if (25 < valor && valor <= 50)
             {
-                Console.WriteLine("Intervalo (25,50]");
+                Console.WriteLine("Intervalo (25, 50]");
             }
 
             else if (50 < valor && valor <= 75)
             {
-                Console.WriteLine("Intervalo (50,75]");
+                Console.WriteLine("Intervalo (50, 75]");
             }
 
             else if (75 < valor && valor <= 100)
             {
-                Console.WriteLine("Intervalo (75,100]");
+                Console.WriteLine("Intervalo (75, 100]");
             }
 
             else
