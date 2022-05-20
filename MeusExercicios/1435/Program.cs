f84069145950502746bcaa70fff3c98ef244177d
Guest 03 <guest03@alunos.sc.senac.br>
2022-05-19 22:25:37 -0300

    Drogas

---
+++ MeusExercicios/1435/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1435
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            int.TryParse(Console.ReadLine(), out numero);
+
+            while (numero !=0)
+            {
+                for (int i = 0; i < numero; i++)
+                {
+
+                }
+            }
+        }
+    }
+}
--- MeusExercicios/1914/Program.cs
+++ MeusExercicios/1914/Program.cs
@@ -6,22 +6,53 @@ namespace _1914
     {
         static void Main(string[] args)
         {
-            int jogados,impar,par, jogada1, jogada2;
-            string somadasjogadas, jogada;
+            int jogados, nome1, nome2, soma, valor1, valor2;
+            string jogada, valores;
 
             int.TryParse(Console.ReadLine(), out jogados);
 
             for (int i = 0; i < jogados; i++)
             {
-                
+              
                 jogada = Console.ReadLine();
-                var vetorJogadas = jogada.Split(' ');
+                var vetorJogada = jogada.Split(' ');
 
-                int.TryParse(vetorJogadas[0], out jogada1);
-                int.TryParse(vetorJogadas[1], out jogada2);
+                int.TryParse(vetorJogada[0], out nome1);
+                int.TryParse(vetorJogada[2], out nome2);
 
+                valores = Console.ReadLine();
+                var vetorValores = valores.Split(' ');
+
+                int.TryParse(vetorValores[0], out valor1);
+                int.TryParse(vetorValores[1], out valor2);
+
+                soma = valor1 + valor2;
+
+                if (soma %2 == 0)
+                {
+                    if (vetorJogada[1] == "PAR")
+                    {
+                        Console.WriteLine($"{vetorJogada[0]}");
+                    }
+                    else if (vetorJogada[3] == "PAR")
+                    {
+                        Console.WriteLine($"{vetorJogada[2]}");
+                    }
+                    
+                }
+                else if (soma %2 == 1)
+                {
+                    if (vetorJogada[1] == "IMPAR")
+                    {
+                        Console.WriteLine($"{vetorJogada[0]}");
+                    }
+                    else if (vetorJogada[3] == "IMPAR")
+                    {
+                        Console.WriteLine($"{vetorJogada[2]}");
+                    }
+
+                }
             }
-
         }
     }
 }
