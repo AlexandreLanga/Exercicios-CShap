34a34728dff60a63698167d664ff4d0e820e0f83
Guest 03 <guest03@alunos.sc.senac.br>
2022-05-24 22:19:31 -0300

    OO

--- MeusExercicios/1103/Program.cs
+++ MeusExercicios/1103/Program.cs
@@ -6,7 +6,18 @@ namespace _1103
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int h1, m1, h2, m2;
+            string numeros;
+
+            numeros = Console.ReadLine();
+            var vetorNumeros = numeros.Split(' ');
+
+            int.TryParse(vetorNumeros[0], out h1);
+            int.TryParse(vetorNumeros[1], out m1);
+            int.TryParse(vetorNumeros[2], out h2);
+            int.TryParse(vetorNumeros[3], out m2);
+
+
         }
     }
 }
---
+++ MeusExercicios/prototipo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace prototipo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //pessoa Um
+            string nomePessoaUm = Console.ReadLine();
+            string sobrenomePessoaUm = Console.ReadLine();
+            int idadePessoaUm = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{nomePessoaUm} {sobrenomePessoaUm}");
+
+            //pessoa dois
+            string nomePessoaDois = Console.ReadLine();
+            string sobrenomePessoasDois = Console.ReadLine();
+            long idadePessoaDois = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{nomePessoaDois}{sobrenomePessoasDois}");
+
+            //pessoa tres
+            string nomePessoaTres = Console.ReadLine();
+            string sobrenomePessoaTres = Console.ReadLine();
+
+            Console.WriteLine($"{nomePessoaTres}{sobrenomePessoaTres}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
