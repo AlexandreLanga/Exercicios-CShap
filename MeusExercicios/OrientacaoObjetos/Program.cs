91c5ea5ee19a58b0b7279453ee1cf70f6b45f0ef
Guest 03 <guest03@alunos.sc.senac.br>
2022-06-13 21:47:12 -0300

    VeiculoClasse

--- MeusExercicios/OrientacaoObjetos/Program.cs
+++ MeusExercicios/OrientacaoObjetos/Program.cs
@@ -6,7 +6,46 @@ namespace OrientacaoObjetos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int funcao = 0;
+
+            do
+            {
+                Console.WriteLine("Olá, seja bem vindo a central de manipulação do seu veiculo!");
+                Console.WriteLine("");
+                Console.WriteLine("Digite qual função deseja aplicar:");
+                Console.WriteLine("(1) Verificar se o veiculo está ligado");
+                Console.WriteLine("(2) Acelerar veiculo");
+                Console.WriteLine("(3) Freiar veiculo");
+                Console.WriteLine("(4) Desligar veiculo");
+                Console.WriteLine("(5) Ligar veiculo");
+                Console.WriteLine("(10) Sair do programa");
+                int.TryParse(Console.ReadLine(), out funcao);  
+
+                if (funcao == 1)
+                {
+                    
+                }
+                if (funcao == 2)
+                {
+
+                }
+                if (funcao == 3)
+                {
+
+                }
+                if (funcao == 4)
+                {
+
+                }
+                if (funcao == 5)
+                {
+
+                }
+                if (funcao == 10)
+                {
+
+                }
+            } while (funcao != 10);
         }
     }
 }
--- MeusExercicios/OrientacaoObjetos/Veiculo.cs
+++ MeusExercicios/OrientacaoObjetos/Veiculo.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace OrientacaoObjetos
 {
-    internal class Class1
+    internal class Veiculo
     {
+        public int VelocidadeAtual { get; set; }
+
+        private bool CarroLigando;
+
+        private void FreiarVeiculo()
+        {
+           VelocidadeAtual = 0;
+        }
+            
+        public void LigarVeiculo()
+        {
+            CarroLigando = true;
+        }
+
+        public void DesligarVeiculo()
+        {
+            if (VelocidadeAtual == 0)
+            {
+                FreiarVeiculo();
+                CarroLigando = false;
+            }
+        }
+
+        public bool VeiculoEsta()
+        {
+            return CarroLigando;
+        }
+
+        public int AcelerarVeiculo(int acelerar)
+        {
+            if (VelocidadeAtual <= 100)
+            {
+                VelocidadeAtual = acelerar;
+            }
+            return VelocidadeAtual;
+        }
     }
 }
