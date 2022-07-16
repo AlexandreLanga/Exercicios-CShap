da1c38844ba511cd109fb8e36aca0dbf84851725
Guest 03 <guest03@alunos.sc.senac.br>
2022-07-15 22:18:29 -0300

    QuasePronto'

---
+++ MeusExercicios/TrabalhoFinal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrabalhoFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
